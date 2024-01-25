6a1c9ea296a336a87470925c66f2632215c9c6e9
Behrozbek Togayev <teramotl@icloud.com>
2024-01-25 21:25:57 +0500

    skeleton added

---
+++ src/main/java/springtodolist/springtodolist/SpringTodolistApplication.java
@@ -0,0 +1,13 @@
+package springtodolist.springtodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTodolistApplication.class, args);
+	}
+
+}
